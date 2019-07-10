5ff491a3663d60a62cbab3cd9f5b7772e80534b8
Troy Garcia <tg1212@outlook.com>
2019-07-10 07:48:45 -0400

    Added Basic File Structure

---
+++ assets/Javascript/script.js
@@ -0,0 +1,56 @@
+// Initial array of movies
+var animals = ["Cat", "Dog", "Mouse"];
+
+// displayMovieInfo function re-renders the HTML to display the appropriate content
+function displayAnimal() {
+
+  var animal = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        animal + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+  // Creating an AJAX call for the specific movie button being clicked
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    var results = response.data;
+    //Clears Div
+    $("#animals-view").empty();
+
+    for (var i = 0; i < results.length; i++) {
+        
+        var animalDiv = $("<div>");
+        var p = $("<p>").text("Rating: " + results[i].rating);
+        var animalImage = $("<img>");
+        animalImage.attr("src", results[i].images.fixed_height.url);
+        animalDiv.append(p);
+        animalDiv.append(animalImage);
+        $("#animals-view").prepend(animalDiv);
+
+    }
+});
+}
+
+function renderButtons() {
+  $("#buttons-view").empty();
+  for (var i = 0; i < animals.length; i++) {
+
+    var a = $("<button>");
+    a.addClass("animal-btn");
+    a.attr("data-name", animals[i]);
+    a.text(animals[i]);
+    $("#buttons-view").append(a);
+  }
+}
+
+$("#add-animal").on("click", function(event) {
+  event.preventDefault();
+  var animal = $("#animal-input").val().trim();
+  animals.push(animal);
+  renderButtons();
+});
+
+$(document).on("click", ".animal-btn", displayAnimal);
+
+// Calling the renderButtons function to display the intial buttons
+renderButtons();
